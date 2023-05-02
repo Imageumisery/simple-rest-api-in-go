6c1b2609824372300e789659b029cf24f8facf9f
imageumisery <imageumisery@gmail.com>
2023-05-02 16:38:29 +0500

    primary key to users

--- migrations/000001_init.up.sql
+++ migrations/000001_init.up.sql
@@ -1,5 +1,5 @@
 CREATE TABLE users (
-    id serial not null unique,
+    id serial not null unique primary key,
     name varchar(255) not null,
     username varchar(255) not null unique,
     password_hash varchar(255) not null
